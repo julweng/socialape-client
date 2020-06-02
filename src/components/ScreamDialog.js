3154bc2e8db0fa508193cba5d946ab78c477d0c8
julweng <julweng@gmail.com>
2020-06-02 15:01:02 -0700

    refactor with useReducer and useDispatch

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -1,70 +1,108 @@
-import React, { useState } from "react";
-import { shape, bool, string, arrayOf } from "prop-types";
-import { connect } from "react-redux";
-import dayjs from "dayjs";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {shape, bool, string, arrayOf} from 'prop-types';
+import {useSelector, useDispatch} from 'react-redux';
+import dayjs from 'dayjs';
+import {Link} from 'react-router-dom';
 // material UI
-import withStyles from "@material-ui/core/styles/withStyles";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
+import withStyles from '@material-ui/core/styles/withStyles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
 // import DialogTitle from "@material-ui/core/DialogTitle";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import CloseIcon from "@material-ui/icons/Close";
-import UnfoldMore from "@material-ui/icons/UnfoldMore";
-import ChatIcon from "@material-ui/icons/Chat";
-// selectors
-import { dataLoadingStatus } from "../redux/reducers/selectors";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import CloseIcon from '@material-ui/icons/Close';
+import UnfoldMore from '@material-ui/icons/UnfoldMore';
+import ChatIcon from '@material-ui/icons/Chat';
+// redux
+import {getScream} from '../redux/actions';
+import {
+  dataLoadingStatus,
+  singleScreamSelector,
+} from '../redux/reducers/selectors';
 // components
-import CommonButton from "../util/commonButton";
-import LikeButton from "./LikeButton"
+import CommonButton from '../util/commonButton';
+import LikeButton from './LikeButton';
 
-const styles = theme => ({
+const styles = (theme) => ({
   ...theme.styles,
   invisibleSeparator: {
-    border: "none",
-    margin: 4
+    border: 'none',
+    margin: 4,
   },
   profileImage: {
     maxWidth: 150,
     height: 150,
-    borderRadius: "50%",
-    objectFit: "cover"
+    borderRadius: '50%',
+    objectFit: 'cover',
   },
   dialogContent: {
-    padding: 20
+    padding: 20,
   },
   closeButton: {
-    position: "absolute",
-    left: "90%",
-    top: "5%"
+    position: 'absolute',
+    left: '90%',
+    top: '5%',
   },
   expandButton: {
-    position: "absolute",
-    left: "90%"
+    position: 'absolute',
+    left: '90%',
   },
   spinnerDiv: {
-    textAlign: "center",
+    textAlign: 'center',
     marginTop: 50,
-    marginBottom: 50
-  }
+    marginBottom: 50,
+  },
 });
 
 const ScreamDialog = ({
   classes,
-  loading,
   likes,
-  scream: { body, createdAt, commentCount, screamId, userHandle, userImage, likeCount }
+  scream: {
+    body,
+    createdAt,
+    commentCount,
+    screamId,
+    userHandle,
+    userImage,
+    likeCount,
+  },
 }) => {
+  const dispatch = useDispatch();
+
   const [open, setOpen] = useState(false);
 
-  const handleOpen = () => setOpen(true);
+  const [oldPath, setOldPath] = useState(window.location.pathname);
 
-  const handleClose = () => setOpen(false);
+  const [newPath, setNewPath] = useState('')
 
-  const renderDialog = loading ? (
-   <div className={classes.spinnerDiv}><CircularProgress size={100} thickness={2}/></div>
+  const handleOpen = () => { 
+    setNewPath(`/users/${userHandle}/scream/${screamId}`);
+
+    if(oldPath === newPath) {
+      setOldPath(`/users/${userHandle}`);
+    }
+
+    setOpen(true);
+
+    window.history.pushState(null, null, newPath);
+    
+    dispatch(getScream(screamId));
+  }
+
+  const handleClose = () => {
+    window.history.pushState(null, null, oldPath);
+    setOpen(false);
+  }
+
+  const handleClick = () => dispatch(getScream(screamId));
+
+  // const singleScream = useSelector((state) => singleScreamSelector(state));
+  
+  const renderDialog = open ? (
+    <div className={classes.spinnerDiv}>
+      <CircularProgress size={100} thickness={2} />
+    </div>
   ) : (
     <Grid container spacing={10}>
       <Grid item sm={3}>
@@ -81,13 +119,13 @@ const ScreamDialog = ({
         </Typography>
         <hr className={classes.invisibleSeparator} />
         <Typography variant="body2" color="textSecondary">
-          {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
+          {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
         </Typography>
         <hr className={classes.invisibleSeparator} />
         <Typography variant="body1">{body}</Typography>
         <LikeButton screamId={screamId} likes={likes} />
         <span>{likeCount} Likes</span>
-        <CommonButton tip="comments">
+        <CommonButton tip="comments" onClick={handleClick}>
           <ChatIcon color="primary" />
         </CommonButton>
         <span>{commentCount} Comments</span>
@@ -128,16 +166,9 @@ ScreamDialog.propTypes = {
     profileImage: string,
     closeButton: string,
     expandButton: string,
-    spinnerDiv: string
+    spinnerDiv: string,
   }).isRequired,
-  likes: arrayOf(shape({})).isRequired
+  likes: arrayOf(shape({})).isRequired,
 };
 
-const mapStateToProps = state => ({
-  loading: dataLoadingStatus(state)
-});
-
-export default connect(
-  mapStateToProps,
-  null
-)(withStyles(styles)(ScreamDialog));
+export default withStyles(styles)(ScreamDialog);
