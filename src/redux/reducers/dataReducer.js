f5ee2743c125743962eb77e6665fb644c4dd95b0
julweng <julweng@gmail.com>
2020-06-07 14:57:00 -0700

    submit comment reducer

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,22 +1,22 @@
-import { get } from "lodash";
-import { createSelector } from "reselect";
-import { createLoadingSelector } from "./loadingReducer";
-import { createErrorSelector } from "./errorReducer";
-import ActionTypes from "../types";
-import updateLikeCount from "./functions/updateLikeCount";
+import {get} from 'lodash';
+import {createSelector} from 'reselect';
+import {createLoadingSelector} from './loadingReducer';
+import {createErrorSelector} from './errorReducer';
+import ActionTypes from '../types';
+import updateLikeCount from './functions/updateLikeCount';
 
 const initialState = {
   screams: [],
   scream: {},
-  dataErrors: {}
+  dataErrors: {},
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case ActionTypes.GET_SCREAMS_SUCCESS:
       return {
         ...state,
-        screams: action.screams
+        screams: action.screams,
       };
     case ActionTypes.LIKE_SCREAM_SUCCESS:
     case ActionTypes.UNLIKE_SCREAM_SUCCESS:
@@ -25,29 +25,37 @@ export default function(state = initialState, action) {
         scream:
           state.scream.screamId === action.scream.screamId
             ? action.scream
-            : state.scream
+            : state.scream,
       };
 
     case ActionTypes.DELETE_SCREAM_SUCCESS:
       return {
         ...state,
-        screams: state.screams.filter(s => s.screamId !== action.screamId)
+        screams: state.screams.filter((s) => s.screamId !== action.screamId),
       };
 
     case ActionTypes.POST_SCREAM_SUCCESS:
       return {
         ...state,
-        screams: [action.scream, ...state.screams]
+        screams: [action.scream, ...state.screams],
       };
     case ActionTypes.POST_SCREAM_FAILURE:
       return {
         ...state,
-        dataErrors: action.err
+        dataErrors: action.err,
       };
     case ActionTypes.GET_SCREAM_SUCCESS:
       return {
         ...state,
-        scream: action.scream
+        scream: action.scream,
+      };
+    case ActionTypes.SUBMIT_COMMENT_SUCCESS:
+      return {
+        ...state,
+        scream: {
+          ...state.scream,
+          comments: [action.data, ...state.scream.comments],
+        },
       };
     default:
       return state;
@@ -55,36 +63,35 @@ export default function(state = initialState, action) {
 }
 
 // selectors
-export const dataStore = rootState => get(rootState, "data", {});
+export const dataStore = (rootState) => get(rootState, 'data', {});
 
 // screams selector
-export const screamsSelector = createSelector(
-  dataStore,
-  state => get(state, "screams", [])
+export const screamsSelector = createSelector(dataStore, (state) =>
+  get(state, 'screams', [])
 );
 
 // single scream Selector
-export const singleScreamSelector = createSelector(
-  dataStore,
-  state => get(state, "scream", {})
+export const singleScreamSelector = createSelector(dataStore, (state) =>
+  get(state, 'scream', {})
 );
 
 // error selector
-export const dataErrorsSelector = createSelector(
-  dataStore,
-  state => get(state, "dataErrors", {})
+export const dataErrorsSelector = createSelector(dataStore, (state) =>
+  get(state, 'dataErrors', {})
 );
 // loading status selector
 export const dataLoadingStatus = createLoadingSelector([
-  "GET_SCREAMS",
-  "POST_SCREAM",
-  "GET_SCREAM"
+  'GET_SCREAMS',
+  'POST_SCREAM',
+  'GET_SCREAM',
+  'SUBMIT_COMMENT',
 ]);
 
 // error status selector
 export const dataErrorStatus = createErrorSelector([
-  "GET_SCREAMS",
-  "UNLIKE_SCREAMS",
-  "LIKE_SCREAMS",
-  "GET_SCREAM"
+  'GET_SCREAMS',
+  'UNLIKE_SCREAMS',
+  'LIKE_SCREAMS',
+  'GET_SCREAM',
+  'SUBMIT_COMMENT',
 ]);
