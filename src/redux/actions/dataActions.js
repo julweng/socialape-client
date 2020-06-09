d9ac4b2585fe9985edcdeb296b4885a84cba6ec4
julweng <julweng@gmail.com>
2020-06-09 13:20:42 -0700

    add getScreams action

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -1,23 +1,61 @@
 import {
-  SET_USER,
+  SET_SCREAMS,
+  LOADING_DATA,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  DELETE_SCREAM,
   SET_ERRORS,
+  POST_SCREAM,
   CLEAR_ERRORS,
   LOADING_UI,
-  SET_UNAUTHENTICATED,
-  LOADING_USER,
-  MARK_NOTIFICATIONS_READ
+  SET_SCREAM,
+  STOP_LOADING_UI,
+  SUBMIT_COMMENT
 } from '../types';
 import axios from 'axios';
 
-export const loginUser = (userData, history) => (dispatch) => {
+// Get all screams
+export const getScreams = () => (dispatch) => {
+  dispatch({ type: LOADING_DATA });
+  axios
+    .get('/screams')
+    .then((res) => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: res.data
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: []
+      });
+    });
+};
+export const getScream = (screamId) => (dispatch) => {
   dispatch({ type: LOADING_UI });
   axios
-    .post('/login', userData)
+    .get(`/scream/${screamId}`)
     .then((res) => {
-      setAuthorizationHeader(res.data.token);
-      dispatch(getUserData());
-      dispatch({ type: CLEAR_ERRORS });
-      history.push('/');
+      dispatch({
+        type: SET_SCREAM,
+        payload: res.data
+      });
+      dispatch({ type: STOP_LOADING_UI });
+    })
+    .catch((err) => console.log(err));
+};
+// Post a scream
+export const postScream = (newScream) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post('/scream', newScream)
+    .then((res) => {
+      dispatch({
+        type: POST_SCREAM,
+        payload: res.data
+      });
+      dispatch(clearErrors());
     })
     .catch((err) => {
       dispatch({
@@ -26,77 +64,75 @@ export const loginUser = (userData, history) => (dispatch) => {
       });
     });
 };
-
-export const signupUser = (newUserData, history) => (dispatch) => {
-  dispatch({ type: LOADING_UI });
+// Like a scream
+export const likeScream = (screamId) => (dispatch) => {
   axios
-    .post('/signup', newUserData)
-    .then((res) => {
-      setAuthorizationHeader(res.data.token);
-      dispatch(getUserData());
-      dispatch({ type: CLEAR_ERRORS });
-      history.push('/');
-    })
-    .catch((err) => {
-      dispatch({
-        type: SET_ERRORS,
-        payload: err.response.data
-      });
-    });
-};
-
-export const logoutUser = () => (dispatch) => {
-  localStorage.removeItem('FBIdToken');
-  delete axios.defaults.headers.common['Authorization'];
-  dispatch({ type: SET_UNAUTHENTICATED });
-};
-
-export const getUserData = () => (dispatch) => {
-  dispatch({ type: LOADING_USER });
-  axios
-    .get('/user')
+    .get(`/scream/${screamId}/like`)
     .then((res) => {
       dispatch({
-        type: SET_USER,
+        type: LIKE_SCREAM,
         payload: res.data
       });
     })
     .catch((err) => console.log(err));
 };
-
-export const uploadImage = (formData) => (dispatch) => {
-  dispatch({ type: LOADING_USER });
+// Unlike a scream
+export const unlikeScream = (screamId) => (dispatch) => {
   axios
-    .post('/user/image', formData)
-    .then(() => {
-      dispatch(getUserData());
-    })
-    .catch((err) => console.log(err));
-};
-
-export const editUserDetails = (userDetails) => (dispatch) => {
-  dispatch({ type: LOADING_USER });
-  axios
-    .post('/user', userDetails)
-    .then(() => {
-      dispatch(getUserData());
-    })
-    .catch((err) => console.log(err));
-};
-
-export const markNotificationsRead = (notificationIds) => (dispatch) => {
-  axios
-    .post('/notifications', notificationIds)
+    .get(`/scream/${screamId}/unlike`)
     .then((res) => {
       dispatch({
-        type: MARK_NOTIFICATIONS_READ
+        type: UNLIKE_SCREAM,
+        payload: res.data
       });
     })
     .catch((err) => console.log(err));
 };
+// Submit a comment
+export const submitComment = (screamId, commentData) => (dispatch) => {
+  axios
+    .post(`/scream/${screamId}/comment`, commentData)
+    .then((res) => {
+      dispatch({
+        type: SUBMIT_COMMENT,
+        payload: res.data
+      });
+      dispatch(clearErrors());
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+export const deleteScream = (screamId) => (dispatch) => {
+  axios
+    .delete(`/scream/${screamId}`)
+    .then(() => {
+      dispatch({ type: DELETE_SCREAM, payload: screamId });
+    })
+    .catch((err) => console.log(err));
+};
 
-const setAuthorizationHeader = (token) => {
-  const FBIdToken = `Bearer ${token}`;
-  localStorage.setItem('FBIdToken', FBIdToken);
-  axios.defaults.headers.common['Authorization'] = FBIdToken;
+export const getUserData = (userHandle) => (dispatch) => {
+  dispatch({ type: LOADING_DATA });
+  axios
+    .get(`/user/${userHandle}`)
+    .then((res) => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: res.data.screams
+      });
+    })
+    .catch(() => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: null
+      });
+    });
+};
+
+export const clearErrors = () => (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
 };
