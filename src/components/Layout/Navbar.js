b9d84f649b61bd9efb2e5a24977b4d4c361b49ed
julweng <julweng@gmail.com>
2020-06-09 13:12:46 -0700

    layout component

---
+++ src/components/Layout/Navbar.js
@@ -0,0 +1,58 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import CommonButton from '../../util/CommonButton';
+import PostScream from '../scream/PostScream';
+import Notifications from './Notifications';
+// MUI stuff
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+// Icons
+import HomeIcon from '@mat../../util/commonButton;
+../Scream/PostScream
+class Navbar extends Component {
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <AppBar>
+        <Toolbar className="nav-container">
+          {authenticated ? (
+            <Fragment>
+              <PostScream />
+              <Link to="/">
+                <CommonButton tip="Home">
+                  <HomeIcon />
+                </CommonButton>
+              </Link>
+              <Notifications />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(Navbar);
