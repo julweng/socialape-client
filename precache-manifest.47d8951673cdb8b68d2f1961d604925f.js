584bc48e963669e26f65ad91964621281c08e1c4
julweng <julweng@gmail.com>
2020-06-10 22:14:53 -0700

    Updates

---
+++ precache-manifest.47d8951673cdb8b68d2f1961d604925f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38c1f5a9f36e427b0608fc77342b63d",
+    "url": "/socialape-client/index.html"
+  },
+  {
+    "revision": "def5fb0fda967c2918af",
+    "url": "/socialape-client/static/css/main.cd9532a2.chunk.css"
+  },
+  {
+    "revision": "dc0a22c711788646b0b5",
+    "url": "/socialape-client/static/js/2.1c88057a.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/socialape-client/static/js/2.1c88057a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def5fb0fda967c2918af",
+    "url": "/socialape-client/static/js/main.f9451de3.chunk.js"
+  },
+  {
+    "revision": "c1a7961737c56f2acff9",
+    "url": "/socialape-client/static/js/runtime-main.f35d5256.js"
+  },
+  {
+    "revision": "f5ef4263a9c36adac59559031d67a0b7",
+    "url": "/socialape-client/static/media/no-img.f5ef4263.png"
+  }
+]);
